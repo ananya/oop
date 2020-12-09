2b73ddb3ee87aae3fb17b5a337594ad8f99a2cb6
ananya <ananyaagrawal125@gmail.com>
2020-12-09 17:03:49 +0530

    class Dog

---
+++ oop.py
@@ -0,0 +1,15 @@
+class Dog:
+    species = "pur"
+    
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name} is {self.age} years old"
+
+    def speak(self, sound):
+        return f"{self.name} says {sound}"
+
+miles = Dog("mile", 4)
+print(miles)
